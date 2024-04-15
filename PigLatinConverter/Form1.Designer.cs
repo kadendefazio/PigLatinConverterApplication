87e24c70494852dc65ab8c10e838dbe2e320e25d
Kaden DeFazio <158978716+kadendefazio@users.noreply.github.com>
2024-04-15 01:07:10 -0400

    Add files via upload

---
+++ PigLatinConverter/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PigLatinConverter
+{
+    partial class Form1 : System.Windows.Forms.Form
+    {
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Location = new System.Drawing.Point(12, 19);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(52, 15);
+            this.InputLabel.TabIndex = 0;
+            this.InputLabel.Text = "Input:";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(70, 16);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(300, 23);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(70, 45);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertButton.TabIndex = 2;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(12, 86);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(57, 15);
+            this.OutputLabel.TabIndex = 3;
+            this.OutputLabel.Text = "Output:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.InputLabel);
+            this.Name = "Form1";
+            this.Text = "Pig Latin Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
